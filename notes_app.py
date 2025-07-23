14e93f69db52ab309dc7124d8220f07bb9351d4f
tushar <tushar.mazumdar2013@gmail.com>
2025-07-23 22:12:42 +0530

    added code to turn on widemode.

--- notes_app.py
+++ notes_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+def wide_space_default():
+    st.set_page_config(layout='wide')
+
+wide_space_default()
+
 with st.sidebar:
     topic = st.selectbox('Choose Topic',(
                 'Data Science',
